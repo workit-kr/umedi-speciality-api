5f0262edf41702f41b3834fcb64b5799ff6fcce1
안혜영 <hyahn@emart.com>
2023-10-08 23:53:39 +0900

    speciality api 추가

---
+++ app.mjs
@@ -0,0 +1,38 @@
+import pg from "pg";
+
+const { Pool } = pg;
+const pool = new Pool({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DB,
+  password: process.env.PG_PASSWORD,
+  port: 5432,
+});
+await pool.connect();
+
+export const handler = async (event) => {
+  const resp = fetchSpecialities()
+  return resp
+}
+
+async function fetchSpecialities() {
+  try {
+    const result = await pool.query('select * from umedi.speciality');
+    if (result.rowCount == 0) {
+      return buildResponse(404, {"message": "no items"})
+    };
+
+    return buildResponse(200, result.rows)
+  }
+  catch (error) {
+    console.error('server error');
+    return buildResponse(500, {"message": "server error"})
+  }
+}
+
+function buildResponse(statusCode, respBody) {
+  return {
+    statusCode: statusCode,
+    body: JSON.stringify(respBody)
+  }
+}
